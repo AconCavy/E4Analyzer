bcc5741d44f43c58b0e6920c7471e63ebd8ec441
Acon <aconcavy@gmail.com>
2020-02-05 19:16:36 +0900

    Add bvp and ibi to empatica_e4.py

--- e4analyzer/empatica_e4.py
+++ e4analyzer/empatica_e4.py
@@ -3,13 +3,23 @@ import numpy as np
 
 class EmpaticaE4:
     _path = ''
+    _bvp = np.empty([1, 0])
+    _ibi = np.empty([2, 0])
     _hr = np.empty([1, 0])
     _eda = np.empty([1, 0])
 
     def __init__(self, path):
         self._path = path
+        self._bvp = np.loadtxt(self._path + '/BVP.csv', skiprows=2, delimiter=",", dtype=float)
+        self._hr = np.loadtxt(self._path + '/IBI.csv', skiprows=1, delimiter=",", dtype=float)
         self._hr = np.loadtxt(self._path + '/HR.csv', skiprows=2, delimiter=",", dtype=float)
         self._eda = np.loadtxt(self._path + '/EDA.csv', skiprows=2, delimiter=",", dtype=float)
+
+    def get_bvp(self):
+        return self._bvp
+
+    def get_ibi(self):
+        return self._ibi
 
     def get_hr(self):
         return self._hr
