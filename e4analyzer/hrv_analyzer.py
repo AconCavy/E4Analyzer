7361c679e97faef2632b9e97f474dee1df38e6f2
Acon <aconcavy@gmail.com>
2020-02-05 21:40:51 +0900

    Fix bugs and add SD of HR

--- e4analyzer/hrv_analyzer.py
+++ e4analyzer/hrv_analyzer.py
@@ -1,5 +1,5 @@
 import numpy as np
-from astropy.stats import lombscargle
+from astropy.timeseries import lombscargle
 
 
 class HRVAnalyzer:
@@ -19,21 +19,21 @@ class HRVAnalyzer:
     def __init__(self, bvp, ibi, hr):
         self._bvp = bvp
         self._ibi = ibi
-        self._ibi[:, 1] = self._ibi[:, 1] * 1000
         self._hr = hr
-
-        print(self._ibi[:, 1])
         self._frequency, self._power = lombscargle.LombScargle(self._ibi[:, 0], self._ibi[:, 1],
                                                                normalization='psd').autopower(method='fast')
 
     def get_nnmean(self):
-        return self._ibi.mean()
+        return self._ibi[:, 1].mean()
 
     def get_sdnn(self):
-        return self._ibi.std(ddof=1)
+        return self._ibi[:, 1].std(ddof=1)
 
     def get_hrmean(self):
         return self._hr.mean()
+
+    def get_hrsd(self):
+        return self._hr.std(ddof=1)
 
     def get_vlf(self):
         band = (self._frequency >= self.VLF_MIN) & (self._frequency < self.VLF_MAX)
@@ -68,7 +68,7 @@ class HRVAnalyzer:
         return self.get_peak_freq(band)
 
     def get_freq(self, band):
-        return np.trapz(y=abs(self._power[band]), x=None, dx=0.1)
+        return np.trapz(y=self._power[band], x=self._frequency[band])
 
     def get_peak_freq(self, band):
         return self._frequency[band][self._power[band].argmax()]
